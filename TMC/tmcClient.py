110828b573abe41f12596b74427fe3160a99b88e
root <Allen-kevin>
2018-09-01 14:24:53 +0800

    update tmcClient.py

--- TMC/tmcClient.py
+++ TMC/tmcClient.py
@@ -15,15 +15,15 @@ recv_count = 0
 max_number = 0
 
 while True:
-	print("client")
-	data, addr = udpCliSock.recvfrom(BUFSIZ)
-	recv_count = recv_count + 1
-	print(data)
-	sum_packet = int(data.decode('UTF-8'))
-	if sum_packet > max_number:
-		max_number = sum_number
-	loss_rate = 1 - recv_count/sum_packet
-	file1 =  open("lossrate.txt", 'a')
-	file1.write(str(recv_count)+'  '+str(sum_packet)+'  '+str(loss_rate)+'\n')
-	file1.close()
+    print("client")
+    data, addr = udpCliSock.recvfrom(BUFSIZ)
+    recv_count = recv_count + 1
+    print(data)
+    sum_packet = int(data.decode('UTF-8'))
+    if sum_packet > max_number:
+    	max_number = sum_packet
+    loss_rate = 1 - recv_count/sum_packet
+    file1 =  open("lossrate.txt", 'a')
+    file1.write(str(recv_count)+'  '+str(sum_packet)+'  '+str(loss_rate)+'\n')
+    file1.close()
 udpCliSock.close()
